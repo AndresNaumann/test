de3b5119e5a217ded23a15ea0acfdba6130f385b
Drew Naumann <naumannadn@gmail.com>
2024-03-08 20:31:54 -0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ncaa from './CollegeBasketballTeams.json';
+
+interface Teams {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+const collegeTeams = ncaa['teams'];
+
+class Team extends React.Component<Teams> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="center-container">
+        <div className="card">
+          <img />
+          <h1>{oneTeam.school}</h1>
+          <h2>Mascot: {oneTeam.name}</h2>
+          <h2>
+            Location: {oneTeam.city}, {oneTeam.state}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {collegeTeams.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
+
+function Heading() {
+  return (
+    <div className="card-heading">
+      <h1>This website is a list of colleges in NCAA Basketball</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <TeamList />
     </div>
   );
 }
